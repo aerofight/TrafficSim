541002d1720c51fd451c629be55dca1fc1fe4e5e
aerofight <105453005+aerofight@users.noreply.github.com>
2024-08-17 20:11:54 -0600

    update

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
-class pr:  #custom printing class
-    def color(text, color):  # the function that allows for the print class to take an input color
-        colorCode = "white";
-        if color == "gray":
-            colorCode = 90
-        elif color == "red":
-            colorCode = 91
-        elif color == "green":
-            colorCode = 92
-        elif color == "yellow":
-            colorCode = 93
-        elif color == "blue":
-            colorCode = 94
-        elif color == "purple":
-            colorCode = 95
-        elif color == "cyan":
-            colorCode = 96
+class Print:  #custom printing class
+    def __init__(self):
+        pass
 
-        print("\033[{}m {}\033[00m".format(colorCode, text))
+    def color(text, color):  # the function that allows for the print class to take an input color
+        color_code = "white"
+        if color == "gray":
+            color_code = 90
+        elif color == "red":
+            color_code = 91
+        elif color == "green":
+            color_code = 92
+        elif color == "yellow":
+            color_code = 93
+        elif color == "blue":
+            color_code = 94
+        elif color == "purple":
+            color_code = 95
+        elif color == "cyan":
+            color_code = 96
+
+        print("\033[{}m {}\033[00m".format(color_code, text))
 
 
 array = ['*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*',
          '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*']
 
-pr.color(" ".join(str(x) for x in array), "green")
+Print.color(" ".join(str(x) for x in array), "green")
